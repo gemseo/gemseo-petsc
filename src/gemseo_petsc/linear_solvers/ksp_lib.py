337fc823104ec7fda1dfb2417a7a97998e8add94
francois.gallard <francois.gallard@irt-saintexupery.com>
2021-09-03 09:54:47 +0200

    tests(Adjoint): improve MDA and adjoint tests using SSBJ

--- src/gemseo_petsc/linear_solvers/ksp_lib.py
+++ src/gemseo_petsc/linear_solvers/ksp_lib.py
@@ -25,64 +25,65 @@ from typing import Any, Dict, List, Tuple, Union
 
 from numpy import ndarray, arange, array, zeros_like
 from scipy.sparse.base import issparse
-from scipy.sparse.linalg import LinearOperator, bicg, bicgstab, gmres, lgmres, qmr
 from scipy.sparse import csr_matrix, find
 from gemseo.algos.linear_solvers.linear_solver_lib import LinearSolverLib
 import petsc4py
 import sys
 
-# Must be done before from petsc4py import PETSc
+# Must be done before from petsc4py import PETSc, this loads the options from
+# command args in the options database.
 petsc4py.init(sys.argv)
 from petsc4py import PETSc
-from matplotlib import pylab
 
 LOGGER = logging.getLogger(__name__)
 
+
 class PetscKSPAlgos(LinearSolverLib):
     """Interface to PETSC KSP
-    
-    For further information, please read 
+
+    For further information, please read
     https://petsc4py.readthedocs.io/en/stable/manual/ksp/
-    
+
     https://petsc.org/release/docs/manualpages/KSP/KSP.html#KSP
-    
+
     """
-    OPTIONS_MAP = { }
+
+    OPTIONS_MAP = {}
 
     def __init__(self):  # type: (...) -> None # noqa: D107
         super(PetscKSPAlgos, self).__init__()
         self.lib_dict = {
             "PETSC_KSP": {
-            self.RHS_MUST_BE_POSITIVE_DEFINITE: False,
-            self.RHS_MUST_BE_SYMMETRIC: False,
-            self.RHS_CAN_BE_LINEAR_OPERATOR: True,
-            self.INTERNAL_NAME: "PETSC",
+                self.RHS_MUST_BE_POSITIVE_DEFINITE: False,
+                self.RHS_MUST_BE_SYMMETRIC: False,
+                self.RHS_CAN_BE_LINEAR_OPERATOR: True,
+                self.INTERNAL_NAME: "PETSC",
             }
         }
 
     def _get_options(
         self,
-        solver_type='gmres',
+        solver_type="gmres",  # type: string
         max_iter=100000,  # type: int
-        tol=1e-5,
-        atol=1e-50,
-        dtol = 1e5,
-        preconditioner_type='ilu',
-        view_config=False,
-        options_hook_func=None,
-        set_from_options=False,
-        monitor_residuals=False,
+        tol=1e-5,  # type: float
+        atol=1e-50,  # type: float
+        dtol=1e5,  # type: float
+        preconditioner_type="ilu",  # type: string
+        view_config=False,  # type: boolean
+        options_hook_func=None,  # type: Optional[boolean]
+        set_from_options=False,  # type: boolean
+        monitor_residuals=False,  # type: boolean
     ):  # type: (...) -> Dict
         """Checks the options and sets the default values.
 
         Args:
             max_iter: Maximum number of iterations.
-            solver_type: The KSP solver type. 
-                See https://petsc.org/release/docs/manualpages/KSP/KSPType.html#KSPType 
+            solver_type: The KSP solver type.
+                See https://petsc.org/release/docs/manualpages/KSP/KSPType.html#KSPType
             tol: The relative convergence tolerance, relative decrease in the (possibly preconditioned) residual norm.
             abstol: The absolute convergence tolerance absolute size of the (possibly preconditioned) residual norm.
             dtol: The divergence tolerance, amount (possibly preconditioned) residual norm can increase.
-            preconditioner_type: The name of the precondtioner, 
+            preconditioner_type: The name of the precondtioner,
                 see https://www.mcs.anl.gov/petsc/petsc4py-current/docs/apiref/petsc4py.PETSc.PC.Type-class.html
             view_config: if True, calls ksp.view() to view the configuration of the solver before run
             options_hook_func: A callback functions that is called with (ksp problem, options dict) as arguments
@@ -105,10 +106,10 @@ class PetscKSPAlgos(LinearSolverLib):
             preconditioner_type=preconditioner_type,
             view_config=view_config,
             set_from_options=set_from_options,
-            options_hook_func=options_hook_func
+            options_hook_func=options_hook_func,
         )
-        
-    def monitor(self, ksp, its, rnorm):
+
+    def __monitor(self, ksp, its, rnorm):
         self.problem.residuals_history.append(rnorm)
 
     def _run(
@@ -129,71 +130,76 @@ class PetscKSPAlgos(LinearSolverLib):
 
         # Initialize ksp solver.
         # Creates the options database
-        options_cmd=options.get("options_cmd")
+        options_cmd = options.get("options_cmd")
         if options_cmd is not None:
             petsc4py.init(options_cmd)
         else:
             petsc4py.init()
         ksp = PETSc.KSP().create()
         ksp.setType(options["solver_type"])
-        ksp.setTolerances(options["tol"],options["atol"], options["dtol"], options["max_iter"])
+        ksp.setTolerances(
+            options["tol"], options["atol"], options["dtol"], options["max_iter"]
+        )
         ksp.setConvergenceHistory()
         a_mat = ndarray2petsc(self.problem.lhs)
         ksp.setOperators(a_mat)
-        prec_type=options.get("preconditioner_type")
+        prec_type = options.get("preconditioner_type")
         if prec_type is not None:
             pc = ksp.getPC()
             pc.setType(prec_type)
             pc.setUp()
-            
+
         # Allow for solver choice to be set from command line with -ksp_type <solver>.
         # Recommended option: -ksp_type preonly -pc_type lu
-        if  options["set_from_options"]  :
+        if options["set_from_options"]:
             ksp.setFromOptions()
-            
-        options_hook_func=options.get('options_hook_func')
+
+        options_hook_func = options.get("options_hook_func")
         if options_hook_func is not None:
             options_hook_func(ksp, options)
-        
-        self.problem.residuals_history=[]
+
+        self.problem.residuals_history = []
         if options["monitor_residuals"]:
-            LOGGER.warning("Petsc option monitor_residuals slows the process and"
-                           " should be used only for testing or convergence studies.")
-            ksp.setMonitor(self.monitor)
+            LOGGER.warning(
+                "Petsc option monitor_residuals slows the process and"
+                " should be used only for testing or convergence studies."
+            )
+            ksp.setMonitor(self.__monitor)
 
         b_mat = ndarray2petsc(self.problem.rhs)
-        solution=b_mat.duplicate()
+        solution = b_mat.duplicate()
         if options["view_config"]:
             ksp.view()
         ksp.solve(b_mat, solution)
         self.problem.solution = solution.getArray()
-        self.problem.convergence_info=ksp.reason
+        self.problem.convergence_info = ksp.reason
         return self.problem.solution
 
 
 def ndarray2petsc(np_arr):
-    n_dim=np_arr.ndim
-    if n_dim>2:
+    n_dim = np_arr.ndim
+    if n_dim > 2:
         raise ValueError("Unsupported dimension {}!".format(n_dim))
-    
+
     if issparse(np_arr):
-        if not isinstance(np_arr,csr_matrix):
-            np_arr=np_arr.tocsr()
-        if n_dim==2 and np_arr.shape[1]>1:
-            petsc_arr = PETSc.Mat().createAIJ(size=np_arr.shape, 
-                                       csr=(np_arr.indptr, np_arr.indices, np_arr.data))
+        if not isinstance(np_arr, csr_matrix):
+            np_arr = np_arr.tocsr()
+        if n_dim == 2 and np_arr.shape[1] > 1:
+            petsc_arr = PETSc.Mat().createAIJ(
+                size=np_arr.shape, csr=(np_arr.indptr, np_arr.indices, np_arr.data)
+            )
             petsc_arr.assemble()
             return petsc_arr
         else:
             petsc_arr = PETSc.Vec().createSeq(np_arr.shape[0])
             petsc_arr.setUp()
-            inds, _, vals=find(np_arr)
-            petsc_arr.setValues(inds,vals)
+            inds, _, vals = find(np_arr)
+            petsc_arr.setValues(inds, vals)
             petsc_arr.assemble()
             return petsc_arr
-             
+
     # Update because of flatten() called in previous line
-    n_dim=np_arr.ndim
+    n_dim = np_arr.ndim
     if n_dim == 1:
         a = array(np_arr, dtype=PETSc.ScalarType)
         petsc_arr = PETSc.Vec().createWithArray(a)
@@ -201,13 +207,16 @@ def ndarray2petsc(np_arr):
         return petsc_arr
     elif n_dim == 2:
         petsc_arr = PETSc.Mat().createDense(np_arr.shape)
-        a_shape=np_arr.shape
+        a_shape = np_arr.shape
         petsc_arr.setUp()
-        petsc_arr.setValues(arange(a_shape[0],dtype="int32"),arange(a_shape[1],dtype="int32"),np_arr)
+        petsc_arr.setValues(
+            arange(a_shape[0], dtype="int32"), arange(a_shape[1], dtype="int32"), np_arr
+        )
         petsc_arr.assemble()
         return petsc_arr
     else:
         raise ValueError("Unsupported dimension {}!".format(n_dim))
+
 
 # KSP example here
 # https://fossies.org/linux/petsc/src/binding/petsc4py/demo/petsc-examples/ksp/ex2.py
--- tests/linear_solvers/test_ksp.py
+++ tests/linear_solvers/test_ksp.py
@@ -26,44 +26,45 @@ from os.path import join, dirname
 from gemseo.algos.linear_solvers.linear_problem import LinearProblem
 from gemseo.algos.linear_solvers.linear_solvers_factory import LinearSolversFactory
 
-from gemseo.api import create_discipline,create_mda
+from gemseo.api import create_discipline, create_mda
 
 from scipy.sparse import load_npz
+
 
 def test_algo_list():
     """Tests the algo list detection at lib creation."""
     factory = LinearSolversFactory()
     assert factory.is_available("PetscKSPAlgos")
     assert factory.is_available("PETSC_KSP")
-    
+
+
 def test_basic():
     random.seed(1)
-    n=3
+    n = 3
     problem = LinearProblem(eye(n), random.rand(n))
     LinearSolversFactory().execute(
         problem,
         "PETSC_KSP",
         max_iter=100000,
         view_config=True,
-        preconditioner_type=None
+        preconditioner_type=None,
     )
     assert problem.residuals(True) < 1e-10
-    
+
+
 @pytest.mark.parametrize("seed", range(3))
 def test_hard_conv(seed):
     random.seed(seed)
     n = 300
     problem = LinearProblem(random.rand(n, n), random.rand(n))
     LinearSolversFactory().execute(
-        problem,
-        "PETSC_KSP",
-        max_iter=100000,
-        view_config=True
+        problem, "PETSC_KSP", max_iter=100000, view_config=True
     )
     assert problem.residuals(True) < 1e-10
- 
-@pytest.mark.parametrize("solver_type", ['gmres','lgmres','fgmres','bcgs'])
-@pytest.mark.parametrize("preconditioner_type", ['ilu','jacobi'])
+
+
+@pytest.mark.parametrize("solver_type", ["gmres", "lgmres", "fgmres", "bcgs"])
+@pytest.mark.parametrize("preconditioner_type", ["ilu", "jacobi"])
 def test_options(solver_type, preconditioner_type):
     random.seed(1)
     n = 3
@@ -73,11 +74,11 @@ def test_options(solver_type, preconditioner_type):
         "PETSC_KSP",
         solver_type=solver_type,
         max_iter=100000,
-        preconditioner_type=preconditioner_type
+        preconditioner_type=preconditioner_type,
     )
     assert problem.residuals(True) < 1e-10
-    
-    
+
+
 def test_residuals_history():
     random.seed(1)
     n = 3000
@@ -87,16 +88,16 @@ def test_residuals_history():
         "PETSC_KSP",
         max_iter=100000,
         preconditioner_type="ilu",
-        monitor_residuals=True
+        monitor_residuals=True,
     )
-    assert len(problem.residuals_history)>=2
+    assert len(problem.residuals_history) >= 2
     assert problem.residuals(True) < 1e-10
-    
+
 
 def test_hard_pb1():
-    lhs=load_npz(join(dirname(__file__),"data","a_mat.npz"))
-    rhs=pickle.load(open(join(dirname(__file__),"data","b_vec.pkl"),'rb'))
-    problem = LinearProblem(lhs,rhs )
+    lhs = load_npz(join(dirname(__file__), "data", "a_mat.npz"))
+    rhs = pickle.load(open(join(dirname(__file__), "data", "b_vec.pkl"), "rb"))
+    problem = LinearProblem(lhs, rhs)
     LinearSolversFactory().execute(
         problem,
         "PETSC_KSP",
@@ -105,19 +106,58 @@ def test_hard_pb1():
         atol=1e-50,
         max_iter=100,
         preconditioner_type="ilu",
-        monitor_residuals=False
+        monitor_residuals=False,
     )
     assert problem.residuals(True) < 1e-3
-    
-    
+
+
 def test_mda_adjoint():
-    disciplines=create_discipline(["SobieskiPropulsion","SobieskiAerodynamics",
-                                   "SobieskiStructure","SobieskiMission"])
-    linear_solver_options={"solver_type":"gmres",
-        "max_iter":100000,
-        "view_config":True}
-    mda=create_mda("MDAChain",disciplines,linear_solver="PETSC_KSP",
-                   linear_solver_options=linear_solver_options)
-    mda.add_differentiated_inputs(["x_shared"])
-    mda.add_differentiated_outputs(["y_4"])
-    mda.linearize()
+    disciplines = create_discipline(
+        [
+            "SobieskiPropulsion",
+            "SobieskiAerodynamics",
+            "SobieskiStructure",
+            "SobieskiMission",
+        ]
+    )
+    linear_solver_options = {
+        "solver_type": "gmres",
+        "max_iter": 100000,
+        "view_config": True,
+    }
+    mda = create_mda(
+        "MDAChain",
+        disciplines,
+        linear_solver="PETSC_KSP",
+        linear_solver_options=linear_solver_options,
+    )
+    assert mda.check_jacobian()
+
+
+def test_mda_newton():
+    disciplines = create_discipline(
+        [
+            "SobieskiPropulsion",
+            "SobieskiAerodynamics",
+            "SobieskiStructure",
+            "SobieskiMission",
+        ]
+    )
+    linear_solver_options = {
+        "solver_type": "gmres",
+        "max_iter": 100000,
+        "view_config": True,
+    }
+
+    tolerance = 1e-13
+    mda = create_mda(
+        "MDANewtonRaphson",
+        disciplines,
+        tolerance=tolerance,
+        linear_solver="PETSC_KSP",
+        linear_solver_options=linear_solver_options,
+    )
+
+    mda.execute()
+    assert mda.residual_history[-1] <= tolerance
+    assert mda.check_jacobian()
