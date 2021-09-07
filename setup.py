629519d261f7bed272c4b78e5d630413c6c5d14a
François Gallard <francois.gallard@irt-saintexupery.com>
2021-09-07 22:15:24 +0200

    First setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+'''
+ * Copyright (c) {2021} {IRT-AESE}.
+ * All rights reserved.
+ *
+ * Contributors:
+ *    {INITIAL AUTHORS} - initial API and implementation and/or initial documentation
+ *        @author: Francois Gallard
+ *    {OTHER AUTHORS}   - {MACROSCOPIC CHANGES}
+'''
+from setuptools import setup, find_packages
+setup(
+    name='gemseo_petsc',
+    version='0.0.1',
+    packages=find_packages('.'),
+    install_requires=[
+        'gemseo',
+        "petsc4py"],
+    include_package_data=True,
+    author="Francois Gallard",
+    author_email='contact@gemseo.org',
+    license='LGPL3',
+    url="www.irt-saintexupery.com"
+)
+
