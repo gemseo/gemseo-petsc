e25b473b6f560a15532d5604d81002bb60ea1ad0
Jean-Christophe Giret <jean-christophe.giret@irt-saintexupery.com>
2021-10-01 14:44:24 +0200

    fix: add missing licence headers

--- tests/linear_solvers/__init__.py
+++ tests/linear_solvers/__init__.py
@@ -12,4 +12,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""This package containes the tests for the  KSP linear solver."""
+"""This package contains the tests for the  KSP linear solver."""
