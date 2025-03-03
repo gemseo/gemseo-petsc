ea8cd822e2691edb83ba4c4c0818f72a7f468392
Giulio Gargantini <giulio.gargantini@irt-saintexupery.com>
2025-03-03 17:09:58 +0100

    Correction type of problem.result.final_state

--- tests/ode/test_ts.py
+++ tests/ode/test_ts.py
@@ -26,15 +26,19 @@ from typing import TYPE_CHECKING
 import pytest
 from gemseo.algos.ode.factory import ODESolverLibraryFactory
 from gemseo.algos.ode.ode_problem import ODEProblem
+from gemseo.typing import RealArray
 from numpy import allclose
 from numpy import arange
 from numpy import array
 from numpy import exp
 from numpy import isclose
+from numpy import ndarray
 from numpy import sqrt
 from numpy import zeros
 
 from gemseo_petsc.ode.ts_library import PetscOdeAlgo
+
+from src.gemseo_petsc.problems.smooth_ode import SmoothODE
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
@@ -335,3 +339,26 @@ def test_failure_snes():
         atol=1e-8,
     )
     assert not problem.result.algorithm_has_converged
+
+def test_check_final_state():
+
+    def create_smooth_ode(initial_state=1.0):
+        return SmoothODE(is_k_design_var=True, initial_state=initial_state)
+
+    problem = create_smooth_ode(1.0)
+
+    ODESolverLibraryFactory().execute(
+        problem,
+        algo_name="PETSC_ODE_RK",
+        time_step=0.01,
+        maximum_steps=1000,
+        compute_adjoint=True,
+        use_jacobian=True,
+        atol=1e-5,
+        rtol=1e-5,
+        use_memory_checkpoints=True,
+        max_disk_checkpoints=100,
+        max_memory_checkpoints=100,
+    )
+
+    assert isinstance(problem.result.final_state, ndarray)
