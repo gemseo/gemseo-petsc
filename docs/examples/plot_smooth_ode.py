54f782d6f3745a5453fd42be7b882c09c7c62669
Giulio Gargantini <giulio.gargantini@irt-saintexupery.com>
2025-03-12 14:14:00 +0100

    advancing in doc examples

--- docs/examples/plot_smooth_ode.py
+++ docs/examples/plot_smooth_ode.py
@@ -30,6 +30,7 @@ from numpy import array
 from numpy import atleast_1d
 from numpy import linspace
 from numpy import zeros
+from src.gemseo_petsc.problems.smooth_ode import SmoothODE
 
 # %%
 # Let us consider the following IVP:
@@ -127,7 +128,7 @@ ODESolverLibraryFactory().execute(
     problem,
     algo_name="PETSC_ODE_BEULER",
     time_step=1e-3,
-    maximum_steps=100000,
+    maximum_steps=1000,
     atol=1e-4,
     use_jacobian=True,
 )
@@ -144,3 +145,20 @@ error = abs(analytical_sol - problem.result.state_trajectories[0])
 plt.semilogy(times, error)
 plt.title("Integration error")
 plt.show()
+
+# %%
+# The `ODEProblem` describing the above ODE is available
+# using the shortcut class `SmoothODE`
+
+shortcut_problem = SmoothODE(
+    initial_state=init_state, times=times, k=k, is_k_design_var=False
+)
+
+ODESolverLibraryFactory().execute(
+    shortcut_problem,
+    algo_name="PETSC_ODE_BEULER",
+    time_step=1e-3,
+    maximum_steps=1000,
+    atol=1e-4,
+    use_jacobian=True,
+)
